4f10367970174d9f0e24e3422941d94a63009904
BillRozy <squee1994@gmail.com>
2024-03-20 15:29:15 +0100

    added usage of email.js

--- components/utility/BackgroundLayer.tsx
+++ components/utility/BackgroundLayer.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { twMerge } from 'tailwind-merge'
 
 export default function BackgroundLayer({ className = '' }: { className?: string }) {
-    return <div className={`absolute -z-50 size-full transition-colors duration-500 ${className}`}></div>
+    const finalClasses = twMerge('absolute -z-50 size-full transition-colors duration-500', className)
+    return <div className={finalClasses}></div>
 }
